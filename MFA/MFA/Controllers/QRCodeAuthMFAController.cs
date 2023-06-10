2d7e2bfd7817ccfb1ee6ea2e44b80401b479a112
Nouf283 <noufawal0311@gmail.com>
2023-06-10 23:42:01 +0600

    qrcode setup api

--- MFA/MFA/Controllers/QRCodeAuthMFAController.cs
+++ MFA/MFA/Controllers/QRCodeAuthMFAController.cs
@@ -6,13 +6,18 @@ using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
+using QRCoder;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Xml.Linq;
 using WebApp.Services;
 
 namespace MFA.Controllers
@@ -38,12 +43,62 @@ namespace MFA.Controllers
             this.EmailMFA = new EmailMFA();
         }
 
+
+        [HttpGet("qrcode-setup")]
+        public async Task<IActionResult> GetQrCodeSetup(string email)
+        {
+            var ViewModel = new SetupMFAViewModel();
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+                return BadRequest("User does not exist");
+            var isTfaEnabled = await _userManager.GetTwoFactorEnabledAsync(user);
+            var key = await _userManager.GetAuthenticatorKeyAsync(user);
+            if (key == null)
+            {
+                await _userManager.ResetAuthenticatorKeyAsync(user);
+                key = await _userManager.GetAuthenticatorKeyAsync(user);
+            }
+            ViewModel.Key = key;
+            ViewModel.QRCodeBytes = GenerateQRCodeBytes("my web app", key, user.Email);
+            return null;
+         
+            //var formattedKey = GenerateQrCode("", authenticatorKey);
+            //return Ok(new TfaSetupDto
+            //{ IsTfaEnabled = isTfaEnabled, AuthenticatorKey = authenticatorKey, FormattedKey = formattedKey });
+        }
+
+
+        private Byte[] GenerateQRCodeBytes(string provider, string key, string userEmail)
+        {
+            var qrCodeGenerater = new QRCodeGenerator();
+            var qrCodeData = qrCodeGenerater.CreateQrCode(
+                $"otpauth://totp/{provider}:{userEmail}?secret={key}&issuer={provider}",
+                QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(20);
+
+            return BitmapToByteArray(qrCodeImage);
+        }
+
+        private Byte[] BitmapToByteArray(Bitmap image)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                image.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
     
 }
 
-public class Credential
-{
-    public string Email { get; set; }
-    public string Securitycode { get; set; }
+    public class SetupMFAViewModel
+    {
+        public string Key { get; set; }
 
-}
+        [Required]
+        [Display(Name = "Code")]
+        public string SecurityCode { get; set; }
+
+        public Byte[] QRCodeBytes { get; set; }
+    }
